885d0808eb46a639f4eebdbc1a200d52181c1a4f
adrianyan0804 <yancy-wm19@student.tarc.edu.my>
2020-09-07 02:05:04 +0800

    COMMIT YCY

--- app/src/main/java/com/example/new_rice_shop/MainActivity.java
+++ app/src/main/java/com/example/new_rice_shop/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.new_rice_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout picJatiSuper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        picJatiSuper = (LinearLayout) findViewById(R.id.image_jati_super);
+
+        picJatiSuper.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,detail_jati_super.class);
+                startActivity(intent);
+
+            }
+        }));
     }
 }
